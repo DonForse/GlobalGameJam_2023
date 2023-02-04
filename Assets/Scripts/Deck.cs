64972eaf879f577c6ae5db44dbb2dc748a523704
AgusEZ <agustinezm@hotmail.com>
2023-02-04 15:47:05 -0300

    Add cards to deck

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -3,10 +3,24 @@
 public class Deck{
     private readonly CardRepositoryScriptableObject _cardRepository;
 
+    public Stack<Card> Cards = new();
+
     public Deck(CardRepositoryScriptableObject cardRepository)
     {
         _cardRepository = cardRepository;
     }
-
-    public Stack<Card> Cards;
+    public void Initialize() => AddCardsToDeck();
+    private void AddCardsToDeck()
+    {
+        AddCardOfType(_cardRepository.FamilyMemberCards);
+        AddCardOfType(_cardRepository.PrincipalOjectiveCards);
+        AddCardOfType(_cardRepository.DeckObjectiveCard);
+        AddCardOfType(_cardRepository.SabotageCards);
+        AddCardOfType(_cardRepository.ShieldCards);
+    }
+    private void AddCardOfType(IEnumerable<Card> cards)
+    {
+        foreach (var card in cards) AddCards(card);
+    }
+    private void AddCards(Card card) => Cards.Push(card);
 }
