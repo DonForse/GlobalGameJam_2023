2c299fe945b7f19e73cccba80e5c4a8e31443c93
AgusEZ <agustinezm@hotmail.com>
2023-02-04 15:28:22 -0300

    Add objective deck card to scriptable repo

--- Assets/Scripts/GameApplication.cs
+++ Assets/Scripts/GameApplication.cs
@@ -1,10 +1,12 @@
-using System.Collections;
 using UnityEngine;
 
 public class GameApplication : MonoBehaviour
 {
+
+    [SerializeField] private CardRepositoryScriptableObject cardsRepo;
     void Start()
     {
-        
+        var deck = new Deck(cardsRepo);
+        //deck.Initialize();
     }
 }
