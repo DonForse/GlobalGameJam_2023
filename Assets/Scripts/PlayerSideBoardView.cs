a71f295f379bc7a4bd7c5df179ab1b5af70472a7
Blas Ingiulla <blas00go@gmail.com>
2023-02-05 01:03:12 -0300

    Show player's and opponent's cards

--- Assets/Scripts/PlayerSideBoardView.cs
+++ Assets/Scripts/PlayerSideBoardView.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Cards.Drag;
 using UnityEngine;
 
@@ -11,12 +12,35 @@ public class PlayerSideBoardView : MonoBehaviour
         childsSlots;
     public void AddCard(Card card, GenerationRow row)
     {
-        Debug.Log($"Add card {card.Name} on row {row.ToString()}");
+        var slot = GetFreeSlot(row);
+        slot.ShowCard(card);
+    }
+
+    private SnappeableSlot GetFreeSlot(GenerationRow generationRow) =>
+        GetSnappeableSlots(generationRow)
+            .First(it => it.IsFree);
+
+    private IEnumerable<SnappeableSlot> GetSnappeableSlots(GenerationRow generationRow) =>
+        GetSlotsFor(generationRow)
+            .Select(it => it.GetComponentInChildren<SnappeableSlot>());
+
+    private List<GameObject> GetSlotsFor(GenerationRow generationRow)
+    {
+        return generationRow switch
+        {
+            GenerationRow.Child => childsSlots,
+            GenerationRow.Parent => parentsSlots,
+            GenerationRow.GrandParent => grandparentsSlots,
+            _ => throw new NotImplementedException($"Tried to play on {generationRow.ToString()}")
+        };
     }
 
     public void RemoveRow(GenerationRow row)
     {
         Debug.Log($"Remove row {row.ToString()}");
-        
+        foreach (var slot in GetSnappeableSlots(row))
+        {
+            slot.Clear();
+        }
     }
 }
