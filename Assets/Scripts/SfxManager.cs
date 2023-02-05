a04505e9ddf7ab245b2ba505959709ff01b09927
Blas Ingiulla <blas00go@gmail.com>
2023-02-05 02:31:50 -0300

    Add play card sfx and play trophie sfx

--- Assets/Scripts/SfxManager.cs
+++ Assets/Scripts/SfxManager.cs
@@ -5,7 +5,7 @@ public class SfxManager : MonoBehaviour
 {
     public AudioSource AudioSource;
 
-    public List<AudioClip>
+    public AudioClip[]
         DrawCardClips,
         PlayCardClips,
         ErrorClips;
@@ -13,4 +13,29 @@ public class SfxManager : MonoBehaviour
     public AudioClip 
         trapClip,
         trophieClip;
+
+    public static void AddTrophy()
+    {
+        Instance.PlayAddTrophy();
+    }
+
+    private void PlayAddTrophy()
+    {
+        AudioSource.PlayOneShot(trophieClip);
+    }
+
+    public static SfxManager Instance => FindObjectOfType<SfxManager>();
+
+    public static void AddCard()
+    {
+        Instance.PlayAddCard();
+    }
+
+    private void PlayAddCard()
+    {
+        var clip = PickRandomClipFrom(PlayCardClips);
+        AudioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip PickRandomClipFrom(AudioClip[] clips) => clips[Random.Range(0, clips.Length)];
 }
